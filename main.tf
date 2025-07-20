9afd16fdb03dcbe3203e617772e5433b2a0bc6af
AmaCaffeinatedCode <amalia.c.villada@gmail.com>
2025-07-20 17:49:02 -0300

    Add root variables and outputs

--- main.tf
+++ main.tf
@@ -1,40 +1,88 @@
 module "tags" {
-  source      = "../modules/tags"
+  source      = "./modules/tags"
   name        = var.name
   environment = var.environment
   project_url = var.project_url
   tags        = var.tags
 }
 
-module "vpc" {
-  source                = "../modules/vpc"
-  name                  = var.name
-  cidr_block            = var.cidr_block
-  azs                   = var.azs
-  private_subnet_cidrs  = var.private_subnet_cidrs
-  endpoint_subnet_cidrs = var.endpoint_subnet_cidrs
+module "security-group" {
+  source      = "./modules/security-group"
+  name        = var.name
+  vpc_id      = module.vpc.vpc_id
+  environment = var.environment
+  project_url = var.project_url
+  tags        = module.tags.tags
+}
 
-  environment  = var.environment
-  project_url  = var.project_url
-  tags         = module.tags.tags
+module "vpc" {
+  source                   = "./modules/vpc"
+  name                     = var.name
+  cidr_block               = var.cidr_block
+  azs                      = var.azs
+  private_subnet_cidrs     = var.private_subnet_cidrs
+  endpoint_subnet_cidrs    = var.endpoint_subnet_cidrs
+  interface_endpoint_sg_ids = [module.security_group.security_group_id] ###
+  environment              = var.environment
+  project_url              = var.project_url
+  tags                     = module.tags.tags
 }
 
 module "s3" {
-  source        = "../modules/s3"
+  source        = "./modules/s3"
   name          = var.name
   environment   = var.environment
   project_url   = var.project_url
   tags          = module.tags.tags
-
   sqs_queue_arn = module.sqs.queue_arn
-  filter_prefix = ""  # Optional S3 key prefix filter for event notifications
+  filter_prefix = ""
 }
 
-
 module "sqs" {
-  source      = "../modules/sqs"
+  source      = "./modules/sqs"
   name        = var.name
   environment = var.environment
   project_url = var.project_url
   tags        = module.tags.tags
+}
+
+module "iam" {
+  source      = "./modules/iam"
+  name        = var.name
+  environment = var.environment
+  project_url = var.project_url
+  tags        = module.tags.tags
+  sqs_queue_arn = module.sqs.queue_arn
+}
+
+module "ecr" {
+  source      = "./modules/ecr"
+  name        = var.name
+  environment = var.environment
+  project_url = var.project_url
+  tags        = module.tags.tags
+}
+
+module "ecs" {
+  source      = "./modules/ecs"
+  name        = var.name
+  environment = var.environment
+  project_url = var.project_url
+  tags        = module.tags.tags
+
+  region              = var.region
+  ecr_image           = module.ecr.ecr_repository_url ###
+
+  sqs_queue_url       = module.sqs.queue_url
+  sqs_queue_arn       = module.sqs.queue_arn
+
+  private_subnet_ids  = module.vpc.private_subnet_ids
+  security_group_id   = module.security_group.security_group_id
+
+  desired_count       = 1
+  min_capacity        = 1
+  max_capacity        = 4
+
+  task_execution_role_arn = module.iam.task_execution_role_arn
+  task_role_arn           = module.iam.task_role_arn
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,28 @@
+# General / Shared variables
 variable "name" {
   description = "Name prefix for all resources"
   type        = string
 }
 
+variable "tags" {
+  description = "Additional tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_url" {
+  description = "URL to the GitHub repository for the project"
+  type        = string
+  default     = ""
+}
+
+# VPC module variables
 variable "cidr_block" {
   description = "VPC CIDR block"
   type        = string
@@ -27,20 +47,44 @@ variable "endpoint_subnet_cidrs" {
   default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
-variable "tags" {
-  description = "Additional tags to apply to resources"
-  type        = map(string)
-  default     = {}
+variable "interface_endpoint_sg_ids" {
+  description = "Security group IDs to attach to VPC interface endpoints"
+  type        = list(string)
+  default     = []
 }
 
-variable "environment" {
-  description = "Deployment environment"
+# IAM module variables
+variable "sqs_queue_arn" {
+  description = "SQS queue ARN for IAM policies"
   type        = string
-  default     = "dev"
 }
 
-variable "project_url" {
-  description = "URL to the GitHub repository for the project"
+# ECS module variables
+variable "region" {
+  description = "AWS region"
   type        = string
-  default     = ""
+  default     = "us-east-1"
+}
+
+variable "ecr_image" {
+  description = "ECR image URI for the ECS task container"
+  type        = string
+}
+
+variable "desired_count" {
+  description = "Initial desired count of ECS tasks"
+  type        = number
+  default     = 1
+}
+
+variable "min_capacity" {
+  description = "Minimum ECS task count for autoscaling"
+  type        = number
+  default     = 1
+}
+
+variable "max_capacity" {
+  description = "Maximum ECS task count for autoscaling"
+  type        = number
+  default     = 4
 }
